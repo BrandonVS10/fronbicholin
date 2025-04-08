a4e4203f8881e62277872f34e2f8fbe632990e40
BrandonVS10 <brandon.vazquez.22e@utzmg.edu.mx>
2025-04-07 19:21:01 -0600

    five bicht

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Login from './components/login/Login';
+import Register from './components/register/Register';
+import SplashScreen from './components/splashScreen/SplashScreen'; // Importa el componente SplashScreen
+import Users from './components/users/Users';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [isSplashDone, setSplashDone] = useState(false);
+
+  // Definir la función onLoaded
+  const onLoaded = () => {
+    setSplashDone(true);  // Al completar el splash, cambia el estado
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onLoaded(); 
+    }, 3000); 
+
+    return () => clearTimeout(timer); // Limpiar el temporizador cuando el componente se desmonte
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {!isSplashDone ? (
+        <SplashScreen onLoaded={onLoaded} /> 
+      ) : (
+        <Routes>
+          <Route path="/" element={<Login />} />
+       {/*    <Route path="/login" element={<Login />} /> */}
+          <Route path="/register" element={<Register />} />
+          <Route path="/users" element={<Users />} />
+        </Routes>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
